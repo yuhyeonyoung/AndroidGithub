fd459b8d2451a05a9057432813f8b6cf0bfbcb85
yuhyeonyoung <hondarr@naver.com>
2023-04-03 19:47:13 +0900

    second commit -- 기능1 추가

---
+++ app/src/main/java/com/example/androidgithub/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidgithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //commit1 -- 기본 기능 구현
+    //commit2 -- 기능1 추가
+}
